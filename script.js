57139f83db1381bec9676a5ba256044383d10ffa
VladimirTi <tytmes@mail.ru>
2019-05-31 00:02:04 +0300

    Vladimir's Solution

---
+++ script.js
@@ -0,0 +1,46 @@
+const posts = fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json());
+
+const users = fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json());
+
+const comments = fetch('https://jsonplaceholder.typicode.com/comments')
+  .then(response => response.json());
+
+
+Promise.all([posts, users, comments])
+  .then(([posts, users, comments]) => {
+    const container = document.querySelector('#container');
+    posts.forEach(post => {
+      const section = document.createElement('section');
+
+      const title = document.createElement('h2');
+      title.innerText = post.title;
+
+      const body = document.createElement('p');
+      body.innerText = post.body;
+
+      const userName = document.createElement('i');
+      userName.innerText = users[post.userId - 1].name;
+
+      const commentsBlock = document.createElement('div')
+      comments.forEach(comment => {
+        if(comment.postId === post.id) {
+          const commentHtml = document.createElement('div');
+          const commentName = document.createElement('h5');
+          const commentText = document.createElement('p');
+          commentName.innerText = comment.name;
+          commentText.innerText = comment.body;
+          commentHtml.append(commentName, commentText);
+          commentsBlock.append(commentHtml);
+        }
+      });
+
+      section.append(title);
+      section.append(body);
+      section.append(userName);
+      section.append(commentsBlock);
+
+      container.append(section);
+    });
+  })
