16e02d35b5eddd430171fbfd54ae7779b0ea0351
Vadim Sirenko <vitalievichkresh@gmail.com>
2019-06-04 20:37:37 +0300

    created post page

--- main.js
+++ main.js
@@ -1,40 +1,65 @@
-'use strict'
+const container = document.querySelector('#root');
+const data = [
+  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholder.typicode.com/comments'    
+];
 
-const urlPosts = 'https://jsonplaceholder.typicode.com/posts';
-// const urlUsers = 'https://jsonplaceholder.typicode.com/users';
-// const urlComments = 'https://jsonplaceholder.typicode.com/comments';
+function createPromise(url) {
+  return fetch(url).then(resp => resp.json());
+}
 
-const requestPosts = new XMLHttpRequest();
-// const requestUsers = new XMLHttpRequest();
-// const requestComments = new XMLHttpRequest();
+function getAllData(urls) {
+  return Promise.all(urls.map(url => createPromise(url)));
+}
 
-const container = document.querySelector('.container');
+function init() {
+  getAllData(data).then((responses) => parseData(responses));
+}
 
-requestPosts.open('GET', urlPosts, false);
-// requestUsers.open('GET', urlUsers, false);
-// requestComments.open('GET', urlComments, false);
+function parseData(dataArrs) {
+  const [posts, users, comments] = dataArrs;
+  createPodos(posts, users, comments);
+}
 
-requestPosts.send();
-// requestUsers.send();
-// requestComments.send();
+document.addEventListener('DOMContentLoaded', init)
 
-console.log(JSON.parse(requestPosts.responseText));
-// console.log(JSON.parse(requestUsers.responseText));
-// console.log(JSON.parse(requestComments.responseText));
+function getUserName(users, id) {
+  return findedObj = users.find(obj => obj.id === id);
+}
 
-const arrayPosts = JSON.parse(requestPosts.responseText)
-// const arrayBodies = JSON.parse(requestUsers.responseText)
-// const arrayPhones = JSON.parse(requestComments.responseText)
+function getComment(comments, id) {
+  const sortedComments = comments.filter(obj => obj.postId === id);
+  console.log(sortedComments);
+  return createComments(sortedComments);
+}
 
-console.log('load')
-for (let i = 0; i < arrayPosts.length; i++) {
-  const post = document.createElement('div');
+function createComments(comments) {
+  const div = document.createElement('div');
+  for (const comment of comments) {
+    div.insertAdjacentHTML('afterbegin', `
+    <div class="comment">
+      <h5>${comment.name}</h5>
+      <p>${comment.body}</p>
+    </div>
+    `)
+  }
+  return div;
+}
 
-  post.innerHTML = `
-  <h2>${arrayPosts[i]['title']}</h2>
-  <div>${arrayPosts[i]['body']}</div>
-  `;
-  // <h4>${arrayPosts[i]['userId']}</h4>
-  
-  container.append(post);
+function createPodos(posts, users, comments) {
+  for (let i = 0; i < posts.length; i++) {
+    const user = getUserName(users, posts[i].userId);
+    const comment = getComment(comments, posts[i].id);
+    const article = document.createElement('article');
+    
+    article.classList.add('wrap');
+    article.insertAdjacentHTML('beforeend', `
+      <h1 class="title">${posts[i].title}</h1>
+      <p>${posts[i].body}</p>
+      <p>${user.name}</p>
+    `)
+    article.append(comment);
+    container.append(article);
+  }
 }
