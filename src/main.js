1f18c51f56143bf9d2e403b74aeead1783f6d457
Ekaterina Kurochka <ekaterina1.kur@gmail.com>
2019-06-06 16:44:17 +0300

    With corrections from TA. With fetch

--- src/main.js
+++ src/main.js
@@ -1,42 +1,50 @@
+'use strict';
+
 const container = document.querySelector('.container');
 
-function CreateTodosList(url) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, false);
-    xhr.send();
-    const parsedData = JSON.parse(xhr.responseText);
-
-    return parsedData;
+function loadApi(url) {
+  return fetch(url)
+    .then(responce => responce.json())
+    .then(data => data)
 }
 
-const posts = CreateTodosList('https://jsonplaceholder.typicode.com/posts');
-const users = CreateTodosList('https://jsonplaceholder.typicode.com/users');
-const comments = CreateTodosList('https://jsonplaceholder.typicode.com/comments');
+function loadItems() {
+  Promise.all([
+    loadApi('https://jsonplaceholder.typicode.com/posts'),
+    loadApi('https://jsonplaceholder.typicode.com/users'),
+    loadApi('https://jsonplaceholder.typicode.com/comments'),
+  ])
+  .then(([posts, users, comments]) => {
+    return render(posts, users, comments);
+  })
+}
+
+loadItems();
 
 function createTemplate(post, user) {
-    return `
-        <h3>${post.title}</h3>
-        <p>${post.body}</p>
-        <i>${user.name}</i>
-        <section id="${post.id}" class="comments"></section>
-    `;
+  return `
+    <h3>${post.title}</h3>
+    <p>${post.body}</p>
+    <i>${user.name}</i>
+    <section id="${post.id}" class="comments"></section>
+  `;
 }
 
 function createComment(comment) {
-    return `
-        <div class="comment">
-            <h4>${comment.name}</h4>
-            <p>${comment.name}</p>
-        </div>
-    `;
+  return `
+    <div class="comment">
+      <h4>${comment.name}</h4>
+      <p>${comment.name}</p>
+    </div>
+  `;
 }
 
-posts.forEach(post => {
+function render(posts, users, comments) {
+  posts.forEach(post => {
     const postWrapper = document.createElement('article');
-
-    const postsAuthor = users.find((author) => post.userId === author.id);
-    const postsComments = comments.filter((comment) => comment.postId === post.id);
- 
+    const postsAuthor = users.find(author => post.userId === author.id);
+    const postsComments = comments.filter(comment => comment.postId === post.id);
+    
     postWrapper.innerHTML = createTemplate(post, postsAuthor);
 
     container.append(postWrapper);
@@ -46,4 +54,5 @@ posts.forEach(post => {
     postsComments.forEach((comment) => {
         commentsSection.insertAdjacentHTML('beforeend', createComment(comment));
     });
-});
+  });
+}
