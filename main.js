ccab0a19d2ddf1f4203aefb2e11d46e733f1a77a
VadimK <djvadya16@gmail.com>
2019-10-24 16:55:04 +0300

    first changes after Ostap advices

--- main.js
+++ main.js
@@ -7,64 +7,57 @@ async function getDate(url) {
   return await response.json(); 
 }
 
-async function listOfPosts() {
-  const posts = await getDate('https://jsonplaceholder.typicode.com/posts'); 
-  const users = await getDate('https://jsonplaceholder.typicode.com/users'); 
-  const comments = await getDate('https://jsonplaceholder.typicode.com/comments');
-
-  // console.log(posts); 
-  // console.log(users); 
-  // console.log(comments); 
-  let postString, author, test, usersComments;
-  let currentComments; 
-  
-
-  for (let i = 0, j = 1; i < posts.length; i++, j++) {
-    postString = ''; 
-    author = users.find((data) => data.id === posts[i].userId).name; 
-    postString += `<div class="authorName"><strong>${author}</strong></div>`; 
-    postString += `<div class="photo"></div>`; 
-    postString += `<div class="title"><strong>${posts[i].title}</strong></div>`;
-    postString += `<div class="bodyText">${posts[i].body}</div>`;  
-    currentComments = comments.filter((id) => id.postId === j); 
-    postString += `<div class="commetsOn"><strong>Watch comments (${currentComments.length})</strong></div>`; 
-    usersComments = '';
-    for (let k = 0; k < currentComments.length; k++) {
-      if (k === currentComments.length - 1) {
-        usersComments += `<p><strong>${currentComments[k].name}</strong>: ${currentComments[k].body}</p>`;
-      } else {
-        usersComments += `<p><strong>${currentComments[k].name}</strong>: ${currentComments[k].body}<hr></p>`;
-      }
+Promise.all([getDate('https://jsonplaceholder.typicode.com/posts'), 
+getDate('https://jsonplaceholder.typicode.com/users'), 
+getDate('https://jsonplaceholder.typicode.com/comments')])
+.then(rezult => {
+  for (let i = 0; i < rezult[0].length; i++) {
+    let post = rezult[0][i]; 
+    let userObj = rezult[1].find((user) => user.id === post.userId); 
+    post.user = userObj; 
+    post.comments = []; 
+    let currentComments = rezult[2].filter((id) => id.postId === post.userId);
+    for (let j = 0; j < currentComments.length; j++) {
+      post.comments.push({}); 
+      post.comments[j].name = currentComments[j].name; 
+      post.comments[j].body = currentComments[j].body; 
     }
-    postString += `<div class="comments">${usersComments}</div>`; 
-    test = document.createElement('div'); 
-    test.classList.add('post'); 
-    test.innerHTML = postString; 
-    postsEnvironment.append(test);
+    let postStr = `
+      <div class="authorName"><strong>${post.user.name}</strong></div>
+      <div class="photo"></div>
+      <div class="title"><strong>${post.title}</strong></div>
+      <div class="bodyText">${post.body}</div>
+      <div class="commentsVisible"><strong>Watch comments 
+      (${post.comments.length})</strong></div>
+    `; 
+    let allComments = ''; 
+    for (let k = 0; k < post.comments.length; k++) {
+      if (k === post.comments.length - 1) {
+        allComments += `<p><strong>${post.comments[k].name}</strong>: 
+          ${post.comments[k].body}</p>`;
+      } else {allComments += `<p><strong>${post.comments[k].name}</strong>: 
+        ${post.comments[k].body}</p><hr>`;}
+       
+    }
+    let mainCommentsDiv = `<div class="comments-main-div" hidden>${allComments}</div>`; 
+    postStr += mainCommentsDiv; 
+    let mainPostDiv = document.createElement('div'); 
+    mainPostDiv.classList.add('post'); 
+    mainPostDiv.innerHTML = postStr; 
+    postsEnvironment.append(mainPostDiv);
   }
 
-  const commentsOn = document.querySelectorAll('.commetsOn'); 
-  const commentsDiv = document.querySelectorAll('.comments'); 
+  //--------------------COMENTS SHOW---------------------------------
+  const commentsVisible = document.querySelectorAll('.commentsVisible'); 
+  const commentsMainDiv = document.querySelectorAll('.comments-main-div'); 
 
-  for (let i = 0; i < commentsOn.length; i++) { 
-    commentsOn[i].addEventListener('click', (e) => {
-      if (!e.target.parentNode.classList.contains('commetsOn')) {
+  for (let i = 0; i < commentsVisible.length; i++) { 
+    commentsVisible[i].addEventListener('click', (e) => {
+      if (!e.target.parentNode.classList.contains('commentsVisible')) {
         return
       }
       
-      if (getComputedStyle(commentsDiv[i]).display === 'none') {
-        commentsDiv[i].style.display = 'block';
-      } else {
-        commentsDiv[i].style.display = 'none';
-      }
+      commentsMainDiv[i].hidden = !commentsMainDiv[i].hidden;
     })
-  }
-};  
-
-listOfPosts(); 
-
-//--------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-
+  }  
+}); 
