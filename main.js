4eb72d57c83045aedf340faaf7b4d07a0e29ce33
VadimK <djvadya16@gmail.com>
2019-10-25 01:01:52 +0300

    done

--- main.js
+++ main.js
@@ -11,43 +11,16 @@ Promise.all([getDate('https://jsonplaceholder.typicode.com/posts'),
 getDate('https://jsonplaceholder.typicode.com/users'), 
 getDate('https://jsonplaceholder.typicode.com/comments')])
 .then(rezult => {
-  for (let i = 0; i < rezult[0].length; i++) {
-    let post = rezult[0][i]; 
-    let userObj = rezult[1].find((user) => user.id === post.userId); 
-    post.user = userObj; 
-    post.comments = []; 
-    let currentComments = rezult[2].filter((id) => id.postId === post.userId);
-    for (let j = 0; j < currentComments.length; j++) {
-      post.comments.push({}); 
-      post.comments[j].name = currentComments[j].name; 
-      post.comments[j].body = currentComments[j].body; 
-    }
-    let postStr = `
-      <div class="authorName"><strong>${post.user.name}</strong></div>
-      <div class="photo"></div>
-      <div class="title"><strong>${post.title}</strong></div>
-      <div class="bodyText">${post.body}</div>
-      <div class="commentsVisible"><strong>Watch comments 
-      (${post.comments.length})</strong></div>
-    `; 
-    let allComments = ''; 
-    for (let k = 0; k < post.comments.length; k++) {
-      if (k === post.comments.length - 1) {
-        allComments += `<p><strong>${post.comments[k].name}</strong>: 
-          ${post.comments[k].body}</p>`;
-      } else {allComments += `<p><strong>${post.comments[k].name}</strong>: 
-        ${post.comments[k].body}</p><hr>`;}
-       
-    }
-    let mainCommentsDiv = `<div class="comments-main-div" hidden>${allComments}</div>`; 
-    postStr += mainCommentsDiv; 
-    let mainPostDiv = document.createElement('div'); 
-    mainPostDiv.classList.add('post'); 
-    mainPostDiv.innerHTML = postStr; 
-    postsEnvironment.append(mainPostDiv);
-  }
+  const [postsList, usersList, commentsList] = rezult; 
 
-  //--------------------COMENTS SHOW---------------------------------
+  for (let i = 0; i < postsList.length; i++) {
+    let currentPost = postsList[i]; 
+    let currentUser = usersList.find(user => user.id === currentPost.userId); 
+    let currentComments = commentsList.filter(comment => 
+      comment.postId === currentPost.id);
+    createPost(currentUser.name, currentPost.title, currentPost.body, currentComments); 
+  }
+//--------------------COMENTS SHOW-----------------------------------------
   const commentsVisible = document.querySelectorAll('.commentsVisible'); 
   const commentsMainDiv = document.querySelectorAll('.comments-main-div'); 
 
@@ -56,8 +29,37 @@ getDate('https://jsonplaceholder.typicode.com/comments')])
       if (!e.target.parentNode.classList.contains('commentsVisible')) {
         return
       }
-      
       commentsMainDiv[i].hidden = !commentsMainDiv[i].hidden;
     })
-  }  
-}); 
+  }
+});
+
+function createPost(name, title, body, comments = []) {
+  let postStr = `
+    <div class="authorName"><strong>${name}</strong></div>
+    <div class="photo"><img src="https://source.unsplash.com/collection/190727/
+      ${Math.round(Math.random() * (450 - 400) + 400)}x
+      ${Math.round(Math.random() * (350 - 300) + 300)}
+      /?food,game,car,nature,animal" class="main-image"></div>
+    <div class="title"><strong>${title}</strong></div>
+    <div class="bodyText">${body}</div>
+    <div class="commentsVisible"><strong>Watch comments 
+    (${comments.length})</strong></div>
+  `; 
+  let allComments = ''; 
+  for (let k = 0; k < comments.length; k++) {
+    if (k === comments.length - 1) {
+      allComments += `<p><strong>${comments[k].name}</strong>: 
+        ${comments[k].body}</p>`;
+    } else {
+      allComments += `<p><strong>${comments[k].name}</strong>: 
+      ${comments[k].body}</p><hr>`;}
+  }
+  let mainCommentsDiv = `<div class="comments-main-div" hidden>${allComments}</div>`; 
+  postStr += mainCommentsDiv;
+
+  let mainPostDiv = document.createElement('div'); 
+  mainPostDiv.classList.add('post'); 
+  mainPostDiv.innerHTML = postStr; 
+  postsEnvironment.append(mainPostDiv);
+}
