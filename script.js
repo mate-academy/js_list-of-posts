f37a63de7c547d3c33debdea447cfe38080e106f
Anna Kazantseva <annetastara@gmail.com>
2019-10-28 16:22:11 +0200

    Fixed

--- script.js
+++ script.js
@@ -1,48 +1,54 @@
 let url = `https://jsonplaceholder.typicode.com`;
 
-let posts = fetch(`${url}/posts`)
-  .then(response => response.json());
+const getUrl = (url) => {
+  return fetch(url)
+    .then(response => response.json());
+};
 
-let users = fetch(`${url}/users`)
-  .then(response => response.json());
+let posts = `${url}/posts`;
+let users = `${url}/users`;
+let comments = `${url}/comments`;
 
-let comments = fetch(`${url}/comments`)
-  .then(response => response.json());
+  Promise.all([
+    getUrl(posts),
+    getUrl(users),
+    getUrl(comments)])
+    .then(([posts, users, comments]) => {
+  let block = document.querySelector('.block');
+  posts.forEach(post => {
 
+    let section = document.createElement('section');
+    section.className = 'ui card';
+    section.style.padding = '2%';
 
-Promise.all([posts, users, comments])
-  .then(([posts, users, comments]) => {
+    let title = document.createElement('h1');
+    title.innerText = post.title;
 
-let block = document.querySelector('.block');
-posts.forEach(post => {
-  let section = document.createElement('section');
-  section.className = 'ui card';
-  section.style.padding = '2%';
-  let title = document.createElement('h1');
-  title.innerText = post.title;
-  let comment = document.createElement('p');
-  comment.innerText = post.body;
-  let author = document.createElement('i');
-  author.innerText = users[post.userId].name;
+    let comment = document.createElement('p');
+    comment.innerText = post.body;
 
-let commentsBlock = document.createElement('div')
-comments.forEach(comment => {
-  if(comment.postId === post.id) {
-    const commentHtml = document.createElement('div');
-    const commentName = document.createElement('h5');
-    const commentText = document.createElement('p');
-    commentName.innerText = comment.name;
-    commentText.innerText = comment.body;
-    commentHtml.append(commentName, commentText);
-    commentsBlock.append(commentHtml);
-  }
+    let author = document.createElement('i');
+    author.innerText = users[post.userId].name;
+
+    let commentsBlock = document.createElement('div')
+    comments.forEach(comment => {
+      if (comment.postId === post.id) {
+        const commentHtml = document.createElement('div');
+        const commentName = document.createElement('h5');
+        const commentText = document.createElement('p');
+        commentName.innerText = comment.name;
+        commentText.innerText = comment.body;
+        commentHtml.append(commentName, commentText);
+        commentsBlock.append(commentHtml);
+      }
+    });
+
+    section.append(title);
+    section.append(comment);
+    section.append(author);
+    section.append(commentsBlock);
+    block.append(section);
+  });
 });
 
-section.append(title);
-section.append(comment);
-section.append(author);
-section.append(commentsBlock);
-block.append(section);
-});
-});
 
