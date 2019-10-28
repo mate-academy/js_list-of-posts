911e55c9f2c017ae48c2355c9ef6e669e1ac3351
Veronika D <donets.veronika@yahoo.com>
2019-10-28 18:16:42 +0200

    fixed js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 'use strict'
 
-const connection = (url) => {
+const getData = (url) => {
   return fetch(url)
     .then(response => response.json());
 };
 
 const posts = async () => {
-  const [posts, users, comments] = await Promise.all(
-    [connection(`https://jsonplaceholder.typicode.com/posts`),
-            connection(`https://jsonplaceholder.typicode.com/users`),
-            connection(`https://jsonplaceholder.typicode.com/comments`)]);
+  const [posts, users, comments] = await
+    Promise.all(
+    [getData(`https://jsonplaceholder.typicode.com/posts`),
+      getData(`https://jsonplaceholder.typicode.com/users`),
+      getData(`https://jsonplaceholder.typicode.com/comments`)
+    ]);
 
   const wrapper = document.querySelector('#wrapper');
   const keys = Object.keys(posts);
@@ -20,32 +22,38 @@ const posts = async () => {
     post.className = 'ui card';
 
     const postsValue = posts[item];
-    const title = posts.find(item => item.id === postsValue.id).title;
-    const body = posts.find(item => item.id === postsValue.id).body;
+    const findPost = posts.find(item => item.id === postsValue.id);
+    const title = findPost.title;
+    const body = findPost.body;
     const userName = users.find(user => user.id === postsValue.userId).username;
 
-    post.innerHTML = `<div class="content">
-                        <div class="header">${title}</div>
-                        </div>
-                        <div class="content">
-                          <div class="ui small feed">${body}</div>
-                        </div>
-                  
-                        <div class="content">
-                          <div class="ui small feed author">
-                            <img class="ui avatar image" src="src/img/${postsValue.userId}.jpg">${userName}
-                          </div>
-                        </div>`;
+    post.innerHTML = `
+      <div class="content">
+        <div class="header">${title}</div>
+      </div>
+      
+      <div class="content">
+        <div class="ui small feed">${body}</div>
+      </div>
+
+      <div class="content">
+        <div class="ui small feed author">
+          <img class="ui avatar image" src="src/img/${postsValue.userId}.jpg">${userName}
+        </div>
+      </div>
+    `;
     wrapper.append(post);
 
     const extraContent = document.createElement('div');
     extraContent.className = 'extra content';
 
-    extraContent.innerHTML = `<div class="content">
-                            <div class="ui comments">
-                              <h3 class="ui dividing header">Comments</h3>
-                            </div>
-                          </div>`;
+    extraContent.innerHTML = `
+      <div class="content">
+        <div class="ui comments">
+          <h3 class="ui dividing header">Comments</h3>
+        </div>
+      </div>
+    `;
     post.append(extraContent);
 
     const pickedCom = comments.filter(item => item.postId === postsValue.id);
@@ -57,13 +65,18 @@ const posts = async () => {
       const singleComment = document.createElement('div');
       singleComment.className = 'comments';
 
-      singleComment.innerHTML = `<div class="content">
-                                   <div class="ui small feed author">${titleCom}</div>                        
-                                  <div class="text">${userCom}</div>
-                                 </div>`;
+      singleComment.innerHTML = `
+        <div class="content">
+          <div class="ui small feed author">${titleCom}</div>                        
+          <div class="text">${userCom}</div>
+        </div>
+      `;
       extraContent.append(singleComment);
     }
   });
 };
 
 posts();
+
+
+
