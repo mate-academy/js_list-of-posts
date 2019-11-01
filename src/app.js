bb780c3a2ab2127b8d0a7604c7afb809e2c21cf8
nikakarelets <v.karelets@gmail.com>
2019-11-01 18:31:39 +0200

    Fix indents

--- src/app.js
+++ src/app.js
@@ -1,26 +1,24 @@
 const fetchData = async function (url) {
-    const response = await fetch(url);
+  const response = await fetch(url);
 
-    return await response.json();
+  return await response.json();
 };
 
 
 const buildPostsList = async () => {
-    Promise.all([fetchData('https://jsonplaceholder.typicode.com/posts'),
-        fetchData('https://jsonplaceholder.typicode.com/users'),
-        fetchData('https://jsonplaceholder.typicode.com/comments')])
-        .then(result => {
-            const [posts, users, comments] = result;
+  Promise.all([fetchData('https://jsonplaceholder.typicode.com/posts'),
+    fetchData('https://jsonplaceholder.typicode.com/users'),
+    fetchData('https://jsonplaceholder.typicode.com/comments')])
+      .then(result => {
+      const [posts, users, comments] = result;
+      const listContainer = document.getElementById('posts');
 
-    const listContainer = document.getElementById('posts');
-
-    posts.forEach((post) => {
+      posts.forEach((post) => {
         const line = document.createElement('li');
         const postTitle = document.createElement('p');
         const postBody = document.createElement('p');
         const postAuthor = document.createElement('p');
         const postAuthorRow = users.find(user => user.id === post.userId);
-
         const postCommentsRows = comments.filter(comment => comment.postId === post.id);
         const postComments = document.createElement('ul');
 
@@ -29,16 +27,12 @@ const buildPostsList = async () => {
         postAuthor.className  = 'post-author';
 
         listContainer.append(line);
-
         line.append(postTitle);
         postTitle.append(post.title);
-
         line.append(postBody);
         postBody.append(post.body);
-
         line.append(postAuthor);
         postAuthor.append(`Author: ${postAuthorRow.name}`);
-
         line.append(postComments);
 
         postCommentsRows.forEach((comment) => {
@@ -52,18 +46,16 @@ const buildPostsList = async () => {
             commentAuthor.className  = 'comment-author';
 
             postComments.append(postCommentsLine);
-
             postCommentsLine.append(commentTitle);
             commentTitle.append(comment.name);
-
             postCommentsLine.append(commentBody);
             commentBody.append(comment.body);
 
             postCommentsLine.append(commentAuthor);
             commentAuthor.append(`Author: ${comment.email}`)
         })
-    })
-        });
+      })
+      });
 };
 
 
